2c6786386b98e66a6b162c280e75989a6b5b895d
Tesgo exp <abdiidris14@gmail.com>
2023-03-16 21:11:31 +0000

    test github actions

---
+++ .github/workflows/create-openssl-keys.sh
@@ -0,0 +1,14 @@
+# install openssl
+apt list -a openssl;
+openssl version -al;
+mkdir ./src/main/resources/certs;
+
+# generate openssl private and primary key
+openssl genpkey -out ./src/main/resources/certs/private-key.pem -algorithm RSA -pkeyopt rsa_keygen_bits:2048
+openssl rsa -in ./src/main/resources/certs/private-key.pem -pubout -out ./src/main/resources/certs/public-key.pem;
+
+head -n 1 ./src/main/resources/certs/public-key.pem;
+head -n 1 ./src/main/resources/certs/private-key.pem;
+
+
+# Test mariadb
